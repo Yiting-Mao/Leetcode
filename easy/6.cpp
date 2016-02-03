55c1e8c34d1a377ca59abd6f571065518d4b9793
Yiting-Mao <rachelym93@gmail.com>
2016-02-03 09:27:19 -0800

    add more efficient solution to 6

--- easy/6.cpp
+++ easy/6.cpp
@@ -1,3 +1,26 @@
+//more advanced version, add characters directly
+class Solution {
+public:
+
+    string convert(string s, int numRows) {
+        int length=s.length();
+        if(length<=numRows||numRows==1)return s;
+        int step=2*(numRows-1);
+        string result;
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<length+step;j=j+step){
+                if(j-i>=0&&j-i<length){
+                    result+=s[j-i];
+                }
+                if(i==0||i==numRows-1)continue;
+                if(j+i<length)result+=s[j+i];
+            }
+        }
+        return result;
+    }
+};
+
+
 class Solution {
 public:
     string convert(string s, int numRows) {
